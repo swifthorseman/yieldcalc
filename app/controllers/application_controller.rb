1845c78fac0263cbf14945aef0c0120492ee37fe
kinnik <northislandking@gmail.com>
2014-11-20 21:12:19 +0000

    Installed Devise for user authentication; generated the user model; installed devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Rails 4.0 and Devise With Strong Params and Custom Fields
+  # http://gorails.com/blog/rails-4-0-and-devise-with-strong-params-and-custom-fields
+  # check to see if the current controller processing the request is one from Devise.
+  # If it is, then we override the parameters that are allowed.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| 
+      u.permit(:full_name, :telephone, :email, :password, :password_confirmation) 
+    }
+  end
+
 end
